202e829ff61a190655f7a5d9aaacc9baf9fc06a2
Anderson Marley <andersonmarley011@gmail.com>
2023-09-14 22:14:06 -0300

    Test da aplicação

---
+++ crud-spring/src/test/java/com/anderson/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
