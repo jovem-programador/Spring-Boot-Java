9654cb5818556683749b96dd3eca42ace559a630
Anderson Marley <andersonmarley011@gmail.com>
2023-09-14 22:13:46 -0300

    Repository - interface extends

---
+++ crud-spring/src/main/java/com/anderson/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.anderson.crudspring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.anderson.crudspring.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long>{
+    
+} 
