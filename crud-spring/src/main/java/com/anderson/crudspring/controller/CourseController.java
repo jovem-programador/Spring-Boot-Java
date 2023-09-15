f23f6b9f03388e50f8cb0621e78d31f3876bde6a
Anderson Marley <andersonmarley011@gmail.com>
2023-09-14 22:12:57 -0300

    Controller - Metodo GET

---
+++ crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.anderson.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anderson.crudspring.model.Course;
+import com.anderson.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
