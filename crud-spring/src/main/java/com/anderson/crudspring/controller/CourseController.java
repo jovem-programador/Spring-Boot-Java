c52eb9b19bee7c0dae9999607fe15e0a41394400
Anderson Marley <andersonmarley011@gmail.com>
2023-09-18 23:16:28 -0300

    Metodo POST criado

--- crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
@@ -2,14 +2,21 @@ package com.anderson.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anderson.crudspring.model.Course;
 import com.anderson.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -21,5 +28,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
