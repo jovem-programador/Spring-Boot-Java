f2d8e046a2bc6f8d9802a41f1f7bdf6a24f5dd71
Anderson Marley <andersonmarley011@gmail.com>
2023-09-27 20:52:03 -0300

    Adicionado o metodo de busca pelo ID

--- crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +28,13 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
