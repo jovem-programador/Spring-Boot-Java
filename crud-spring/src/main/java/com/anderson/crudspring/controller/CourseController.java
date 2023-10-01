8a06abba2a27ad9ff414cdac9629874f293c66a8
Anderson Marley <andersonmarley011@gmail.com>
2023-09-30 22:11:24 -0300

    Metodo PUT e DELETE adicionado

--- crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/anderson/crudspring/controller/CourseController.java
@@ -3,8 +3,8 @@ package com.anderson.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +17,7 @@ import com.anderson.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -34,12 +35,38 @@ public class CourseController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable Long id) {
-        return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
+        return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound)).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+
+        return courseRepository.findById(id).map(recordFound -> {
+
+            recordFound.setName(course.getName());
+            recordFound.setCategory(course.getCategory());
+
+            Course updated = courseRepository.save(recordFound);
+
+            return ResponseEntity.ok().body(updated);
+
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        return courseRepository.findById(id).map(recordFound -> {
+
+            courseRepository.deleteById(id);
+            return ResponseEntity.noContent().<Void>build();
+
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
