750f34f685d429dead877ba1bec5e8267153c8cf
Anderson Marley <andersonmarley011@gmail.com>
2023-10-11 18:26:45 -0300

    Tratamento de exeção

--- crud-spring/src/main/java/com/anderson/crudspring/service/CourseService.java
+++ crud-spring/src/main/java/com/anderson/crudspring/service/CourseService.java
@@ -1,14 +1,12 @@
 package com.anderson.crudspring.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import com.anderson.crudspring.exception.RecordNotFoundException;
 import com.anderson.crudspring.model.Course;
 import com.anderson.crudspring.repository.CourseRepository;
 
@@ -33,8 +31,8 @@ public class CourseService {
     }
 
     // GET ID
-    public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     // POST
@@ -43,7 +41,7 @@ public class CourseService {
     }
 
     // UPDATE
-    public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+    public Course update(@NotNull @Positive Long id, @Valid Course course) {
 
         return courseRepository.findById(id).map(recordFound -> {
 
@@ -52,18 +50,13 @@ public class CourseService {
 
             return courseRepository.save(recordFound);
 
-        });
+        }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     // DELETE
-    public boolean delete(@PathVariable @NotNull @Positive Long id) {
+    public void delete(@PathVariable @NotNull @Positive Long id) {
 
-        return courseRepository.findById(id)
-
-        .map(recordFound -> {
-            courseRepository.deleteById(id);
-            return true;
-        })
-        .orElse(false);
+        courseRepository.delete(courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id)));
     }
 }
