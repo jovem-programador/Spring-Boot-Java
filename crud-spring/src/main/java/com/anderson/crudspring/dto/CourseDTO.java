ddbb66ade135a14a489555e493a3b42bb4abf8c4
Anderson Marley <andersonmarley011@gmail.com>
2023-10-27 15:24:50 -0300

    Logica de Aulas adicionadas

--- crud-spring/src/main/java/com/anderson/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/anderson/crudspring/dto/CourseDTO.java
@@ -1,5 +1,7 @@
 package com.anderson.crudspring.dto;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,7 +18,8 @@ public record CourseDTO(
 
         @JsonProperty("_id") long id,
         @NotBlank @NotNull @Length(min = 2, max = 250) String name,
-        @NotNull @Pattern(regexp = "Back-End|Front-End") @Length(min = 5, max = 250) String category
+        @NotNull @Pattern(regexp = "Back-End|Front-End") @Length(min = 5, max = 250) String category,
+        List<LessonDTO> lessons
 
 ) {
 
---
+++ crud-spring/src/main/java/com/anderson/crudspring/dto/LessonDTO.java
@@ -0,0 +1,9 @@
+package com.anderson.crudspring.dto;
+
+public record LessonDTO(
+    Long id,
+    String name,
+    String youtubeUrl
+) {
+    
+}
